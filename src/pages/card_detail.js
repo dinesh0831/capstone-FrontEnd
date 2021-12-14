a954f8bd39b8345e0f8ae48beb482c87b5473904
Dinesh <81325951+dinesh0831@users.noreply.github.com>
2021-12-14 17:52:47 +0530

    Add files via upload

--- src/pages/card_detail.js
+++ src/pages/card_detail.js
@@ -9,7 +9,9 @@ import jwt from "jsonwebtoken"
 import DateRangePicker from '@mui/lab/DateRangePicker'
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import {backEndUrl} from "../backend"
+import { backEndUrl } from "../backend"
+
+import frLocale from 'date-fns/locale/fr';
 
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -26,8 +28,9 @@ function CardDetail() {
     const [photos, setPhotos] = useState([])
     const [amenities, setAmenities] = useState([])
     const [highlight, setHighlight] = useState([])
-   
-   
+    const [book, setBook] = useState("")
+
+
     const params = useParams()
     const [value, setValue] = React.useState([null, null]);
     const [state, setState] = useState({
@@ -42,7 +45,7 @@ function CardDetail() {
         setState({ ...state, [anchor]: open });
     };
     const token = localStorage.getItem("clone")
-    const history=useHistory()
+    const history = useHistory()
     const getPost = async () => {
         const { data } = await axios.get(`${backEndUrl}/getpost/getone/${params.id}`)
         setValues(data)
@@ -58,7 +61,7 @@ function CardDetail() {
 
         getPost()
 
-    },[])
+    },)
     const loggedIn = () => {
 
         return localStorage.getItem("clone") ? localStorage.removeItem("clone") : history.push("/login")
@@ -67,46 +70,49 @@ function CardDetail() {
         console.log(history)
         return localStorage.getItem("clone") ? history.push("/profile") : history.push("/login")
     }
-    const bookNow =async () => {
+    const bookNow = async () => {
         if (token) {
             const decode = jwt.decode(token)
             const response = await axios.get(`${backEndUrl}/getpost/getone/${params.id}`)
-           const booking={
-                    startDate: value[0],
-                    endDate: value[1],
-                    bookingStatus:"Booked",
-                    bookingdId:decode.user._id}
-               
-            let booked=[...response.data.booked]
-            console.log(booked)
-           await booked.push(booking)
+            const booking = {
+                startDate: value[0],
+                endDate: value[1],
+                bookingStatus: "Booked",
+                bookingdId: decode.user._id
+            }
 
-               await axios.patch(`${backEndUrl}/posts/update/${params.id}`, {
+            let booked = [...response.data.booked]
+            console.log(booked)
+            await booked.push(booking)
+
+            await axios.patch(`${backEndUrl}/posts/update/${params.id}`, {
                 booked
             },
                 {
                     headers: { clone: token }
                 })
-            const {data}= await axios.get(`${backEndUrl}/users/profile/${decode.user._id}`)
-            const userBooking={
+            const { data } = await axios.get(`${backEndUrl}/users/profile/${decode.user._id}`)
+            const userBooking = {
                 startDate: value[0],
                 endDate: value[1],
-                bookingStatus:"Booked",
-                bookingId:params.id,
-                title:values.title,}
-               
-                const bookedlist=[...data.bookedList]
-               await bookedlist.push(userBooking)
-
-                await axios.patch(`${backEndUrl}/users/wishlist/${decode.user._id}`,{
-                bookedList:bookedlist
-                })
+                bookingStatus: "Booked",
+                bookingId: params.id,
+                title: values.title,
             }
-            
-           else {
+
+            const bookedlist = [...data.bookedList]
+            await bookedlist.push(userBooking)
+
+            await axios.patch(`${backEndUrl}/users/wishlist/${decode.user._id}`, {
+                bookedList: bookedlist
+            })
+            setBook( " *Your Booking Request successfully Send. please Check your Profile for more detail")
+        }
+
+        else {
             history.push("/login")
-            
-             }
+
+        }
 
     }
     const list = (anchor) => (
@@ -163,16 +169,16 @@ function CardDetail() {
 
     return (
         <Box>
-             <Box sx={{ position: "absolute", right: 0, height: 70, display: "flex", alignItems: "center", }}>
-                        <Button onClick={toggleDrawer("right", true)}><MenuIcon sx={{ fontSize: 24, color: "black", }} /></Button>
-                        <Drawer
-                            anchor={"right"}
-                            open={state.right}
-                            onClose={toggleDrawer("right", false)}
-                        >
-                            {list("right")}
-                        </Drawer>
-                </Box>
+            <Box sx={{ position: "absolute", right: 0, height: 70, display: "flex", alignItems: "center", }}>
+                <Button onClick={toggleDrawer("right", true)}><MenuIcon sx={{ fontSize: 24, color: "black", }} /></Button>
+                <Drawer
+                    anchor={"right"}
+                    open={state.right}
+                    onClose={toggleDrawer("right", false)}
+                >
+                    {list("right")}
+                </Drawer>
+            </Box>
             <Box sx={{ width: "100%", height: 70, display: "flex", alignItems: "center", justifyContent: "center", overflow: "hidden", }}>
 
                 <Box component={Link} to={"/"} sx={{ display: "flex", justifyContent: "center", alignItems: "center", textDecoration: "none", color: "black", padding: 2, '&:hover': { textDecoration: "underline" } }}><HomeIcon sx={{ fontSize: 24 }} /> <Typography sx={{ fontSize: 24, fontWeight: "bold", }}> Home</Typography></Box>
@@ -180,7 +186,7 @@ function CardDetail() {
                 <Box component={Link} to={"/host"} sx={{ display: "flex", justifyContent: "center", alignItems: "center", textDecoration: "none", color: "black", padding: 2, '&:hover': { textDecoration: "underline" } }}><PostAddIcon sx={{ fontSize: 24 }} /><Typography sx={{ fontSize: 24, fontWeight: "bold", }}> Host your place</Typography></Box>
             </Box>
             <Box sx={{ display: "flex" }}>
-                <Box sx={{ marginLeft: 5, width: "60%" }}>
+                <Box sx={{ marginLeft: 5, width: "70%",border:.5,borderRadius:2,marginRight:5,padding:5 }}>
                     <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
                         {photos.map((item) => (
                             <ImageListItem key={item.originalname}>
@@ -217,7 +223,7 @@ function CardDetail() {
                                             sx={{
                                                 typography: { fontSize: 16 }, display: "flex", justifyContent: "center", alignItems: "center", border: 0.3,
                                                 borderColor: "black", color: "black", margin: 1, borderRadius: 3,
-                                                bgcolor: "white", padding:3, marginLeft: 3, height: 30,  
+                                                bgcolor: "white", padding: 3, marginLeft: 3, height: 30,
 
                                             }}>
 
@@ -247,7 +253,7 @@ function CardDetail() {
                                             sx={{
                                                 typography: { fontSize: 16 }, display: "flex", justifyContent: "center", alignItems: "center", border: 0.3,
                                                 borderColor: "black", color: "black", margin: 1, borderRadius: 3,
-                                                bgcolor: "white", marginLeft: 3, height: 30, padding:3,
+                                                bgcolor: "white", marginLeft: 3, height: 30, padding: 3,
 
                                             }}>
 
@@ -260,26 +266,27 @@ function CardDetail() {
                             </Box>
                         </Box>
                         <Box>
-                        <Typography gutterBottom variant="h5" component="div">Type of place:</Typography>
-                        <Box sx={{marginLeft: 3}}>
-                        <Typography gutterBottom variant="h6" component="div">
-                            Property type: {values.houseType}
-                        </Typography>
-                        <Typography gutterBottom variant="h6" component="div">
-                            Place to share: {values.kindOfShare}
-                        </Typography>
-                        
-                                
-                        </Box>
+                            <Typography gutterBottom variant="h5" component="div">Type of place:</Typography>
+                            <Box sx={{ marginLeft: 3 }}>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    Property type: {values.houseType}
+                                </Typography>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    Place to share: {values.kindOfShare}
+                                </Typography>
+
+
+                            </Box>
 
                         </Box>
 
                     </Box>
 
                 </Box>
-                <Box sx={{ width: "40%", height: "50%", padding: 1, border: 1, borderRadius: 2 }}>
-                    <Box className="date_picker">
-                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Box sx={{ width: "30%", height: "50%", padding: 1, border: 1, borderRadius: 2,marginRight:5 }}>
+                    
+                    <Box className="date_picker" >
+                        <LocalizationProvider dateAdapter={AdapterDateFns} locale={frLocale}>
                             <DateRangePicker
                                 startText="Check-in"
                                 endText="Check-out"
@@ -296,9 +303,12 @@ function CardDetail() {
                                 )}
                             />
                         </LocalizationProvider>
-                        <Box sx={{display:"flex",justifyContent:"center",margin:2}}>
+                        <Box sx={{ display: "flex", justifyContent: "center", margin: 2 }}>
                             <Button onClick={bookNow}>Book Now</Button>
                         </Box>
+                        
+                        <Typography sx={{color:"green",border:"black",textAlign:"center" }}>{book}</Typography>
+                        
 
                     </Box>
 
